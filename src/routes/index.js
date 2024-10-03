09229abf931391ba1a3f665737d264ad1d2655d0
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-10-04 05:24:16 +0530

    resolved doctor bugs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import patientRoutes from "../routes/patientRoutes.js";
 import adminRoutes from "../routes/adminRoutes.js";
 import hospitalRoutes from "../routes/hospitalRoutes.js";
+import doctorRoutes from "../routes/doctorRoutes.js";
 
 const router = express.Router();
 
 router.use("/admin", adminRoutes);
 router.use("/patient", patientRoutes);
+router.use("/doctor", doctorRoutes);
 router.use("/hospital", hospitalRoutes);
 
 export default router;
