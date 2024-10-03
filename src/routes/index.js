9f7ec4ca903ce729e39f292b51cf0831443b7b27
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-10-03 13:46:17 +0530

    create-hospital api done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import patientRoutes from "../routes/patientRoutes.js";
 import adminRoutes from "../routes/adminRoutes.js";
+import hospitalRoutes from "../routes/hospitalRoutes.js";
+
 const router = express.Router();
 
-router.use("/patient", patientRoutes);
 router.use("/admin", adminRoutes);
+router.use("/patient", patientRoutes);
+router.use("/hospital", hospitalRoutes);
 
 export default router;
