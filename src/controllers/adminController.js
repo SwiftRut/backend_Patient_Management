e2c058e6543fadb97343d2e374bcfe044b88ba8a
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-10-04 22:58:11 +0530

    patient bug solved and extra field added

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -96,7 +96,7 @@ export const loginAdmin = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: admin._id, role: }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: admin._id, role: "admin"}, process.env.JWT_SECRET, {
       expiresIn: rememberMe ? "7d" : "1d",
     });
 
