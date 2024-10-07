49b54ef275c286dbe5b4dfb205a8d0cdfdd97cc7
Mayur Hapani <127928054+mayurhapani@users.noreply.github.com>
2024-10-07 22:11:00 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,13 +10,6 @@ import universalRoutes from "../routes/universalRoutes.js"
 import insuranceRoutes from "./insuranceRoutes.js";
 import aggregationRoutes from "../routes/aggregationRoutes.js";
 
-import adminModel from "../models/adminModel.js";
-import doctorModel from "../models/doctorModel.js";
-import patientModel from "../models/patientModel.js";
-import { loginAdmin } from "../controllers/adminController.js";
-import { loginDoctor } from "../controllers/doctorController.js";
-import { loginPatient } from "../controllers/patientController.js";
-
 const router = express.Router();
 
 router.use("/",universalRoutes);
@@ -30,52 +23,6 @@ router.use("/aggregation", aggregationRoutes);
 router.use("/insurance", insuranceRoutes);
 router.use("/appoinment", appoinmentRoutes);
 router.use("/presciption", prescriptionRoutes);
-router.post("/universal-login", async (req, res) => {
-  const { identifier, password, rememberMe } = req.body;
-
-  if (!identifier || !password) {
-    return res
-      .status(400)
-      .json({ message: "Please provide email/phone and password" });
-  }
-
-  try {
-    const normalizedIdentifier = identifier.trim().toLowerCase();
-    const normalizedPhone = identifier.trim().replace(/[\s\+\-\(\)]/g, "");
-    console.log("Normalized Identifier:", normalizedIdentifier);
-    console.log("Normalized Phone:", normalizedPhone);
-    const admin = await adminModel.findOne({
-      $or: [{ email: normalizedIdentifier }, { phone: normalizedPhone }],
-    });
-
-    if (admin) {
-      return loginAdmin(req, res);
-    }
-
-    const doctor = await doctorModel.findOne({
-      $or: [{ email: normalizedIdentifier }, { phone: normalizedPhone }],
-    });
-
-    if (doctor) {
-      return loginDoctor(req, res);
-    }
-
-    const patient = await patientModel.findOne({
-      $or: [{ email: normalizedIdentifier }, { phone: normalizedPhone }],
-    });
-    console.log("Patient:", patient);
-    console.log("Doctor:", doctor);
-    console.log("Admin:", admin);
-    if (patient) {
-      return loginPatient(req, res);
-    }
-
-    return res.status(400).json({ message: "Invalid credentials" });
-  } catch (error) {
-    console.error("Error during universal login:", error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 
 export default router;
