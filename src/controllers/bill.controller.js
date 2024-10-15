e4e7b521111d6bf53e2f10af21254cb73f5c4943
mohitdudhat22 <mohitdudhat22@gmail.com>
2024-10-15 21:39:18 +0530

    bill backend

--- src/controllers/bill.controller.js
+++ src/controllers/bill.controller.js
@@ -1,39 +1,85 @@
 import billModel from "../models/billModel.js";
-
+import insuranceModel from "../models/insuranceModel.js";
+import patientModel from "../models/patientModel.js";
 export const createBill = async (req, res) => {
-  console.log("hangle the create a Bill")
-    try {
-        const {
-            billNumber,
-            patientId,
-            description,
-            paymentType,
-            date,
-            time,
-            amount,
-            discount,
-            tax,
-            totalAmount,
-            insuranceId
-        } = req.body
-        console.log(req.body)
-        // if (!billNumber || !description || !paymentType || !date || !time || !amount || !discount || !tax || !totalAmount) {
-        //     return res.status(400).json({ message: "All fields are required" });
-        //   }
+  console.log("hangle the create a Bill");
+  try {
+    const {
+      billNumber,
+      description,
+      paymentType,
+      billDate:date,
+      billTime:time,
+      amount,
+      discount,
+      tax,
+      totalAmount,
+      insuranceCompany,
+      insurancePlan,
+      claimAmount,
+      claimedAmount,
+      doctorId,
+      doctorName,
+      phoneNumber: phone,
+      patientName,
+      gender,
+      age,
+      diseaseName,
+      address,
+    } = req.body;
+    console.log(req.body, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Bill Creation");
+    //find the patient id based on the phone
+    const patient = await patientModel.findOne({ phone });
+    console.log(patient, "<<<<<<<<<<<<<<<<<<<<<<<<<<< Patient ID");
 
-        const newBill = new billModel(req.body)
-        await newBill.save();
-        res.status(201).json({
-            success: true,
-            data: newBill,
-        });
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            message: 'Failed to create the bill',
-            error: error.message,
-        });
+    if (insuranceCompany && insurancePlan && claimAmount && claimedAmount) {
+      const newInsurance = new insuranceModel({
+        patientId : patient._id,
+        insuranceCompany,
+        insurancePlan,
+        claimAmount,
+        claimedAmount,
+      });
+      console.log(newInsurance,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< insurance company")
+      await newInsurance.save();
+      req.body.insuranceId = newInsurance._id;
     }
+    // if (!billNumber || !description || !paymentType || !date || !time || !amount || !discount || !tax || !totalAmount) {
+    //     return res.status(400).json({ message: "All fields are required" });
+    //   }
+
+    const newBill = new billModel({
+      billNumber,
+      phone,
+      gender,
+      age,
+      patientId : patient._id,
+      doctorId,
+      insuranceId:req.body.insuranceId,
+      diseaseName,
+      description,
+      paymentType,
+      date,
+      time,
+      amount,
+      discount,
+      tax,
+      totalAmount,
+      address,
+      patientName
+    });
+    await newBill.save();
+    res.status(201).json({
+      success: true,
+      data: newBill,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Failed to create the bill",
+      error: error.message,
+    });
+  }
 };
 
 export const getBills = async (req, res) => {
@@ -78,40 +124,43 @@ export const getBillById = async (req, res) => {
     });
   }
 };
-  export const updateBill = async (req, res) => {
-    try {
-      console.log(req.body);
-  
-      if (req.body.totalAmount && isNaN(Number(req.body.totalAmount))) {
-        return res.status(400).json({
-          success: false,
-          message: 'Invalid totalAmount value',
-        });
-      }
-  
-      const updatedBill = await billModel.findByIdAndUpdate(req.params.id, req.body, {
+export const updateBill = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    if (req.body.totalAmount && isNaN(Number(req.body.totalAmount))) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid totalAmount value",
+      });
+    }
+
+    const updatedBill = await billModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
         new: true,
         runValidators: true,
-      });
-  
-      if (!updatedBill) {
-        return res.status(404).json({
-          success: false,
-          message: 'Bill not found',
-        });
       }
-  
-      res.status(200).json({
-        success: true,
-        data: updatedBill,
-      });
-    } catch (error) {
-      res.status(500).json({
+    );
 
+    if (!updatedBill) {
+      return res.status(404).json({
         success: false,
         message: "Bill not found",
       });
     }
+
+    res.status(200).json({
+      success: true,
+      data: updatedBill,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Bill not found",
+    });
+  }
 };
 
 export const deleteBill = async (req, res) => {
