c8bbff7ab67b38c689ee5f3b1ac2a2cf4497c700
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-10-22 00:32:02 +0530

    auth route bug solved for appointment

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router()
 
 //here we have to change the role bbase on the role of the user
 router.post("/appoinmentcreate" ,protect, authorize(["patient"]), createAppointment)
-router.get("/allappoinment" , protect, authorize(["patient", "doctor"]), AllAppointment)
-router.put("/updateappointment/:id" ,protect, authorize(["patient", "doctor"]), UpdateAppointment)
+router.get("/allappoinment" , protect, authorize(["admin","patient", "doctor"]), AllAppointment)
+router.put("/updateappointment/:id" ,protect, authorize(["admin","patient", "doctor"]), UpdateAppointment)
 router.delete("/deleteappointment/:id" ,protect,authorize(["patient", "doctor"]) , DeleteAppointment)
 router.get("/Patient_Appointment_History/:PatientID" , protect, authorize(["patient", "doctor"]) , getPatientAppointmentHistory)
 router.get("/Doctor_Appointment_History/:id",protect ,authorize(["patient", "doctor"]) , getDoctorAppointmentHistory);
