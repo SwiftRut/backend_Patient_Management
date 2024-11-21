950fc47b5043f252be54398e1e39d6df0e3c0283
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-11-21 23:34:04 +0530

    redis implimenrtation in socket

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,12 +1,31 @@
+import { CACHE_TIMEOUT } from "../constants.js";
 import Chat from "../models/chatModel.js";
 import doctorModel from "../models/doctorModel.js";
 import patientModel from "../models/patientModel.js";
+import { client } from "../redis.js";
+const invalidateCache = async (id) => {
+  const CACHE_KEYS = {
+    GET_DOCTOR_BY_ID: `/doctor/getDoctorById/${id}`,
+    GET_DOCTOR_APPOINTMENT_HISTORY: `/appointment/getDoctorAppointmentHistory/${id}`,
+  };
+
+  const keysToDelete = Object.values(CACHE_KEYS); // Extract all keys to delete
+
+  for (const key of keysToDelete) {
+    await client.del(key); // Delete each key from the cache
+  }
+
+  console.log(`Cache invalidated for keys: ${keysToDelete.join(", ")}`);
+};
 
 // Create a new message
 const createMessage = async (req, res) => {
   const { senderId, receiverId, messageContent } = req.body;
   try {
-    const chat = new Chat({ senderId, receiverId, messageContent });
+    const chat = new Chat({ senderId, receiverId, messageContent })
+    invalidateCache(senderId);
+    invalidateCache(receiverId);
+    await client.del(`/chat/${senderId}/${receiverId}`);
     await chat.save();
     res.status(201).json(chat);
   } catch (error) {
@@ -21,6 +40,9 @@ const getChatHistory = async (req, res) => {
     const history = await Chat.find({ doctorId, patientId }).sort({
       timestamp: 1,
     });
+
+    const key = req.originalUrl;
+    await client.setEx(key, CACHE_TIMEOUT, JSON.stringify(history));
     res.status(200).json(history);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -36,6 +58,9 @@ const updateMessageStatus = async (req, res) => {
       { status: "read" },
       { new: true }
     );
+    const chat = new Chat({ senderId, receiverId, messageContent })
+    invalidateCache(senderId);
+    invalidateCache(receiverId);
     res.status(200).json(updatedChat);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -46,6 +71,9 @@ const deleteMessage = async (req, res) => {
   const { chatId } = req.params;
   try {
     await Chat.findByIdAndDelete(chatId);
+    const chat = new Chat({ senderId, receiverId, messageContent })
+    invalidateCache(senderId);
+    invalidateCache(receiverId);
     res.status(200).json({ message: "Message deleted" });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -57,10 +85,14 @@ const getDoctorContacts = async (req, res) => {
   const { patientId } = req.params;
   try {
     const doctorIds = await Chat.find({ patientId }).distinct("doctorId");
-    const doctorContacts = await doctorModel.find({
-      _id: { $in: doctorIds },
-    }).select("-password");
+    const doctorContacts = await doctorModel
+      .find({
+        _id: { $in: doctorIds },
+      })
+      .select("-password");
 
+    const key = req.originalUrl;
+    await client.setEx(key, CACHE_TIMEOUT, JSON.stringify(doctorContacts));
     res.status(200).json(doctorContacts);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -71,10 +103,14 @@ const getPatientContacts = async (req, res) => {
   const { doctorId } = req.params;
   try {
     // Step 1: Get distinct patient IDs
-const patientIds = await Chat.find({ doctorId }).distinct("patientId");
+    const patientIds = await Chat.find({ doctorId }).distinct("patientId");
 
-// Step 2: Populate the patient details using the IDs
-const populatedPatients = await patientModel.find({ _id: { $in: patientIds } });
+    // Step 2: Populate the patient details using the IDs
+    const populatedPatients = await patientModel.find({
+      _id: { $in: patientIds },
+    });
+    const key = req.originalUrl;
+    await client.setEx(key, CACHE_TIMEOUT, JSON.stringify(populatedPatients));
     res.status(200).json(populatedPatients);
   } catch (error) {
     res.status(500).json({ error: error.message });
--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import { cacheMiddleware } from "../middlewares/cacheMiddleware.js";
 const router = express.Router();
 import { createMessage, getChatHistory, updateMessageStatus, deleteMessage, getDoctorContacts, getPatientContacts } from '../controllers/chatController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 // CRUD operations for chat messages
 router.post('/', createMessage);
-router.get('/:doctorId/:patientId',getChatHistory);
+router.get('/:doctorId/:patientId',protect,cacheMiddleware,getChatHistory);
 router.put('/:chatId', updateMessageStatus);
 router.delete('/:chatId', deleteMessage);
 
 // Endpoints to retrieve contacts list
-router.get('/contacts/patient/:patientId', getDoctorContacts);
-router.get('/contacts/doctor/:doctorId', getPatientContacts);
+router.get('/contacts/patient/:patientId',protect,cacheMiddleware,getDoctorContacts);
+router.get('/contacts/doctor/:doctorId',protect,cacheMiddleware, getPatientContacts);
 export default router;
